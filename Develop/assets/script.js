fc3b6b74a9f74295fd4af730a9f295a1b606fbb6
Jose Lemuz <lemuz.jose@gmail.com>
2022-06-27 00:13:44 -0700

    added formatting and saving tasks to array

---
+++ Develop/assets/script.js
@@ -0,0 +1,36 @@
+
+//Creates the date at the top of the form
+var oneDate = moment();
+var time = oneDate.format('dddd, MMMM Do');
+document.querySelector('#currentDay').textContent = time;
+
+console.log(oneDate.format('k'));
+
+var times = document.getElementsByTagName("textarea");
+var len = times.length;
+
+for(i=0;i<len;i++){
+    id = times[i].id;
+     if(parseInt(id,10)<oneDate.format('k')){
+        document.getElementById(id).setAttribute("class", "description past");
+     }
+     else if(parseInt(id,10)>oneDate.format('k')){
+        console.log(id, oneDate.format('k'))
+        document.getElementById(id).setAttribute("class", "description future");
+     }
+     else if (parseInt(id,10)==oneDate.format('k')){
+        document.getElementById(id).setAttribute("class", "description present");
+     }
+};
+
+var tasks = [];
+
+$('.bi').click(function(){
+    taskNum = $(this).attr('id').match(/\d+/)[0];
+    task = $('#'+taskNum);
+    task[0].textContent = task[0].value
+    tasks.filter(tasks)
+    tasks.push(taskNum, task[0].value)
+    let uniqueCity = [...new Set(tasks)]
+    console.log(uniqueCity);
+});
